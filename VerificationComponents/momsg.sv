3960d51a0f0d4942488cfd5c504b15acc2f0f852
Tyler(Zihe) Deng <tyler.dzh@gmail.com>
2021-05-10 12:47:19 -0700

    update

--- VerificationComponents/momsg.sv
+++
@@ -1,7 +0,0 @@
-// This is the output message class from the DUT:
-// This class has not yet plug into the system yet
-// I use this class for frame scoreboards: and data_frame_out_scoreboard
-class momsg;
-	logic reset, startout, pushout;
-	logic [9:0] dataout;
-endclass
